8063cc3a75b073b26f14629997dca7207ff9126c
Dayv <Borovsky.kruskoff@hotmail.fr>
2019-03-08 19:17:49 +0100

    Start page done

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -7,12 +7,13 @@ void create_block(void *adr, size_t size, void *next, void *prev)
 	block->size = size;
 	block->is_free = true;
 	block->next = next;
-	block->previous = previous
+	block->previous = previous;
+	block->next->previous = block;
+	block->previous->next = block;
 }
 
-void *first_page(t_page *first, t_page *next, t_page *prev size_t size)
+void *first_page(t_page *first, t_mem_block *first_block, size_t size)
 {
-	void *return_adr;
 	size_t mult = 1;
 	size_t sizeblock = sizeof(t_mem_block);
 	size_t sizepage = sizeof(t_page);
@@ -24,13 +25,31 @@ void *first_page(t_page *first, t_page *next, t_page *prev size_t size)
 	first->next = next;
 	first->prev = prev;
 	first->page_num = 1;
-	create_block(first + sizepage, size);
+	first_block->size = size;
+	first_block->is_free = false;
+	first_block->previous = first_block;
+	first_block->next = first_block;
+	if (PAGESIZE * mult - (size + sizepage + sizeblock) > sizeblock)
+	{
+		create_block(size + sizepage + sizeblock,
+			PAGESIZE * mult - (size + sizepage + sizeblock * 2),
+			first_block, first_block);
+	}
+	return (first_block + sizeblock);
+}
+
+void *test_free(void *adr, size_t size, t_page *first, t_mem_block *first_block)
+{
+
 }
 
 void *malloc(void *adr, size_t size)
 {
-	static t_page *first;
+	static t_mem_block *first_block;
+	static t_page *first_page;
 
-	if (!(first->next))
-		new_page(first, first, first, size);
+	if (!(first_page->next))
+		return (first_page(first_page, first_block, size));
+	else
+		return(test_free(adr, size, first_page, first_block));
 }
