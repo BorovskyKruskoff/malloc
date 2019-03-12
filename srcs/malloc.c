84de344d4cf7f094b4ad4f5abe725e975b9218db
Dayv <Borovsky.kruskoff@hotmail.fr>
2019-03-12 18:06:59 +0100

    QoL and coding style improvements

--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,6 +1,6 @@
 #include "malloc.h"
 
-void create_block(void *adr, size_t size, void *next, void *prev)
+void init_block(void *adr, size_t size, void *next, void *prev)
 {
 	t_mem_block *block = adr;
 
@@ -12,14 +12,13 @@ void create_block(void *adr, size_t size, void *next, void *prev)
 	block->prev->next = block;
 }
 
-void *create_first_page(t_page *first, t_mem_block *first_block, size_t size)
+void *init_first_page(t_page *first, t_mem_block *first_block, size_t size)
 {
 	size_t mult = 1;
 	size_t sizeblock = sizeof(t_mem_block);
 	size_t sizepage = sizeof(t_page);
 
-	while (PAGESIZE * mult < size + sizeblock + sizepage)
-		mult += 1;
+	mult = ((size + sizeblock + sizepage) / PAGESIZE) + 1;
 	first = mmap(first, PAGESIZE * mult, PROT_NONE,
 		MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 	first->next = first;
@@ -33,7 +32,7 @@ void *create_first_page(t_page *first, t_mem_block *first_block, size_t size)
 	first_block->next = first_block;
 	if (PAGESIZE * mult - (size + sizepage + sizeblock) > sizeblock)
 	{
-		create_block((char *)(first_block + sizeblock + size),
+		init_block((char *)(first_block + sizeblock + size),
 			PAGESIZE * mult - (size + sizepage + sizeblock * 2),
 			first_block, first_block);
 	}
@@ -48,8 +47,7 @@ void *new_page(size_t size, t_page *old_page)
 	size_t sizeblock = sizeof(t_mem_block);
 	size_t sizepage = sizeof(t_page);
 
-	while (PAGESIZE * mult < size + sizeblock + sizepage)
-		mult += 1;
+	mult = ((size + sizeblock + sizepage) / PAGESIZE) + 1;
 	new_page = mmap(NULL, PAGESIZE * mult, PROT_NONE,
 		MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 	new_page->next = old_page->next;
@@ -65,7 +63,7 @@ void *new_page(size_t size, t_page *old_page)
 	new_block->next = new_block;
 	if (PAGESIZE * mult - (size + sizepage + sizeblock) > sizeblock)
 	{
-		create_block((char *)(new_block + sizeblock + size),
+		init_block((char *)(new_block + sizeblock + size),
 			PAGESIZE * mult - (size + sizepage + sizeblock * 2),
 			new_block, new_block);
 	}
@@ -81,7 +79,7 @@ void take_block(t_mem_block *current, size_t size)
 	current->size = size;
 	if (size_left > sizeblock)
 	{
-		create_block((char *)(current + sizeblock + current->size), 
+		init_block((char *)(current + sizeblock + current->size), 
 			size_left - sizeblock, current->next, current);
 	}
 }
@@ -95,7 +93,7 @@ void *find_free_block(size_t size, t_page *first_page, t_mem_block *first_block)
 
 	while (searching)
 	{
-		if (test_block->size >= size && test_block->is_free == true)
+		if (test_block->size >= size && test_block->is_free)
 		{
 			take_block(test_block, size);
 			searching = false;
@@ -121,7 +119,7 @@ void *malloc(size_t size)
 	static t_page *first_page;
 
 	if (!(first_page->next))
-		return (create_first_page(first_page, first_block, size));
+		return (init_first_page(first_page, first_block, size));
 	else
 		return (find_free_block(size, first_page, first_block));
 }
